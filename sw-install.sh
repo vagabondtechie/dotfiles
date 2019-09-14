cfddd8dd50f7675e2078f169c5ba390b5b1ad8af
vagabondtechie <vagabondtechie@gmail.com>
2019-09-14 21:38:24 +0530

    Made misc changes
    Changes:
     1. Added an empty line before .git-specific file
     2. Added a sw-install.sh script which will copy and/or
        run the dot files

---
+++ sw-install.sh
@@ -0,0 +1,11 @@
+# Development software
+sudo apt-get install python3-pip python-pip virtualenv git openssh-server openssh-client g++ gcc
+
+# Archives and restricted extras
+sudo apt-get install unace rar unrar p7zip-rar p7zip sharutils uudeview mpack arj cabextract lzip lunzip gdebi
+
+# General purpose software
+sudo apt-get install libreoffice vlc firefox
+
+cat .git-specific >> ~/.bashrc
+cp .vimrc ~/
